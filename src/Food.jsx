2237000ef15673d1b29e119154e859b4619a523b
Pankaj Baswal <pankajbaswal3360@gmail.com>
2025-05-27 11:58:03 +0530

    added few components

---
+++ src/Food.jsx
@@ -0,0 +1,12 @@
+function Food(){
+   let food1="Mango";
+   let food2 = "Apple";
+    return(
+     <ul>
+        <li>Banana</li>
+        <li>{food1}</li>
+        <li>{food2}</li>
+     </ul>
+    )
+}
+export default Food;
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+     return(
+        <header>
+            <p>New Website</p>
+            <nav>
+                <ul>
+                    <li> <a href="#"> Home </a> </li>
+                    <li><a href="#">ABout Us </a></li>
+                    <li><a href="#"> Services </a></li>
+                </ul>
+            </nav>
+        </header>
+     )
+}   
+
+export default Header
